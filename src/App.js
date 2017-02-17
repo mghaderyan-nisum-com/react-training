67d6538d174f8aa91f5a88fac3a417a5df748743
wnqnguo <wnqnguo@gmail.com>
2017-02-17 15:23:46 -0800

    'life-cycle-methods'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Cart from './components/Cart'
 
 const App = React.createClass( {
 	getInitialState: function(){
-	return { items: [{
+		console.log("getInitialState");
+		return { items: [{
 					    title: 'Womens Zitah Pointed Toe Pumps',
 					    qty: 1,
 					    total: 50,
@@ -22,7 +23,12 @@ const App = React.createClass( {
 					  },]};
 
 	},
+ componentWillMount: function(){
+	 console.log("componentWillMount");
+ },
   render() {
+		console.log("render");
+    this.manualUpdate();
     return (
       <div className="App">
         <h2>Shopping Card App</h2>
@@ -30,6 +36,9 @@ const App = React.createClass( {
       </div>
     );
   },
+	componentDidMount: function(){
+		console.log("componentDidMount");
+	},
   handleChange:function(item,value){
   	var allItems = this.state.items.filter(function(element){
   		if(element.title==item.title){
@@ -42,6 +51,43 @@ const App = React.createClass( {
   handleUpdate:function(){
 
   },
-});
+  manualUpdate:function(){
+  function callback(){
+      var allItems = [{
+							    title: 'Womens Zitah Pointed Toe Pumps',
+							    qty: 1,
+							    total: 50,
+							  },
+							  {
+							    title: 'Womens Basel Booties',
+							    qty: 5,
+							    total: 150,
+							  },
+							  {
+							    title: 'Jamie T-Strap Platform Dress Sandals',
+							    qty: 2,
+							    total: 100,
+							  }];
 
+
+     this.setState({items: allItems});
+
+    }
+    setTimeout(callback.bind(this),3000);
+  },
+	shouldComponentUpdate: function(nextProps,nextState){
+		if(this.state.items[1].qty!==nextState.items[1].qty){
+			console.log("shouldComponentUpdate is true");
+			return true;
+		}
+		console.log("shouldComponentUpdate is false");
+		return false;
+	},
+	componentWillUpdate: function(nextProps,nextState){
+		console.log("componentWillUpdate");
+	},
+	componentDidUpdate:function(){
+		console.log("componentDidUpdate");
+	}
+});
 export default App;
